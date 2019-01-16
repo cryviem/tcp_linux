174453186621521325b80951ff48cff35471630c
nguyen hong an <hanguyen@luxoft.com>
2019-01-16 04:55:25 -0800

    sync to home 1

--- sync/tsap_srvr.c
+++ sync/tsap_srvr.c
@@ -5,35 +5,21 @@
  *      Author: cryviem
  */
 
-#include <errno.h>
-
 #include "tsap/tsap.h"
 #include "tsap/tsap_srvr.h"
 #include "common/commonthing.h"
 
-static threadpool svr_thpl_hlr;
-
 
 int16_t server_init(uint16_t port, uint16_t maxclnt)
 {
-	int ret, fdflags, socketid;
-	psrvrhdlr_t	psrvrhdlr;
+	int ret, fdflags, socketid, err = 0;
 	struct sockaddr_in servaddr;
-	/*create socket*/
+	psrvrhdlr_t	psrvrhdlr;
+
 	START_SEGMENT
-
+	/*create socket*/
 	socketid = socket(AF_INET, SOCK_STREAM, 0);
-	EXIT_IF(socketid == -1, printf("tsap: create socket fail!\n"))
-
-	if (socketid == -1)
-	{
-		printf("tsap: create socket fail!\n");
-		break;
-	}
-	else
-	{
-		printf("tsap: create socket successful!\n");
-	}
+	EXIT_IF(socketid == -1, err = -1)
 
 	/*bind IP and PORT address*/
 	bzero(&servaddr, sizeof(servaddr));
@@ -42,124 +28,183 @@ int16_t server_init(uint16_t port, uint16_t maxclnt)
 	servaddr.sin_port = htons(port);
 
 	ret = bind(socketid, (struct sockaddr*)&servaddr, sizeof(servaddr));
-	if (ret != 0)
-	{
-		printf("tsap: bind socket fail!\n");
-		return TSAP_RET_NOK;
-	}
-	else
-	{
-		printf("tsap: bind socket successful!\n");
-	}
+	EXIT_IF(ret, err = -2)
 
 	/*turn socket file descriptor to non-blocking*/
 	fdflags = fcntl(socketid, F_GETFL, 0);
-	if (fdflags < 0)
-	{
-		printf("tsap: fcntl get flag fail!\n");
-		return TSAP_RET_NOK;
-	}
-	else
-	{
-		printf("tsap: fcntl get flag successful!\n");
-	}
+	EXIT_IF(fdflags == -1, err = -3)
 
 	ret = fcntl(socketid, F_SETFL, fdflags | O_NONBLOCK);
-	if (ret < 0)
-	{
-		printf("tsap: fcntl set non-blocking fail!\n");
-		return TSAP_RET_NOK;
-	}
-	else
-	{
-		printf("tsap: fcntl set non-blocking successful!\n");
-	}
+	EXIT_IF(ret == -1, err = -4)
 
 	/*start listen for connection*/
 	ret = listen(socketid, maxclnt);
-	if (ret < 0)
-	{
-		printf("tsap: start listen fail!\n");
-		return TSAP_RET_NOK;
-	}
-	else
-	{
-		printf("tsap: listening...\n");
-	}
+	EXIT_IF(ret == -1, err = -5)
 
 	/*create server handler*/
 	psrvrhdlr = (psrvrhdlr_t)malloc(sizeof(serverhandler_type));
-	if (psrvrhdlr == NULL)
-	{
-		printf("tsap: server handler allocate fail!\n");
-		return TSAP_RET_NOK;
-	}
-	else
-	{
-		printf("tsap: server handler allocate successful\n");
-	}
-
+	EXIT_IF(psrvrhdlr == NULL, err = -6)
+	/*initialize*/
+	bzero(psrvrhdlr, sizeof(serverhandler_type));
 	/*Prepare handler data*/
 	psrvrhdlr->socketid = socketid;
 
 	/*create thread pool*/
-	svr_thpl_hlr = thpool_init((maxclnt+1));
-	if (svr_thpl_hlr == NULL)
-	{
-		printf("tsap: thread pool create fail!\n");
-		free(psrvrhdlr);
-		return TSAP_RET_NOK;
-	}
-	else
-	{
-		printf("tsap: thread pool is created\n");
-	}
+	psrvrhdlr->thpoolinst = thpool_init((maxclnt+1));
+	EXIT_IF(psrvrhdlr->thpoolinst == NULL, err = -7)
 
 	/*run check connection task*/
-	ret = thpool_add_work(svr_thpl_hlr, (void*)svrproc_checknewconnection, psrvrhdlr);
-	if (ret == -1)
-	{
-		printf("tsap: add task check_new_connection fail!\n");
-		free(psrvrhdlr);
-		thpool_destroy(svr_thpl_hlr);
-		return TSAP_RET_NOK;
-	}
-	else
-	{
-		printf("tsap: add check_new_connection successful\n");
-	}
+	ret = thpool_add_work(psrvrhdlr->thpoolinst, (void*)svrproc_checknewconnection, psrvrhdlr);
+	EXIT_IF(ret == -1, err = -8)
 
 	END_SEGMENT
 
+	if (err < 0)
+	{
+		/*got error, destroy section*/
+		free(psrvrhdlr);
+		thpool_destroy(svr_thpl_hlr);
+		//call disconnected_cb with con_id invalid
+	}
+	return err;
 }
 
 void svrproc_checknewconnection(psrvrhdlr_t srvrhdlr)
 {
 	int ret;
+	int16_t clientindx;
 	struct sockaddr_in clientaddr;
+	srvrhdlr->clientindx = MAX_CLIENT_SP;
 	while(1)
 	{
-		bzero(&clientaddr, sizeof(clientaddr));
-		ret = accept(srvrhdlr->socketid, (struct sockaddr *)&clientaddr, sizeof(clientaddr));
-		if(ret >= 0)
+		if (srvrhdlr->clientindx < MAX_CLIENT_SP)
 		{
-			/*new connection*/
-
+			/*previous communication task hasn't response*/
+			continue;
 		}
-		else
+
+		clientindx = get_clientbox(srvrhdlr->clientlist);
+		if (clientindx >= 0)
 		{
-			if (errno != EWOULDBLOCK && errno != EAGAIN)
+			bzero(&clientaddr, sizeof(clientaddr));
+			ret = accept(srvrhdlr->socketid, (struct sockaddr *)&clientaddr, sizeof(clientaddr));
+			if(ret >= 0)
 			{
-				/*system error*/
-				LOG("tsap: accept() got error: %s\n", strerror(errno));
-				break;
+				/*new connection*/
+				srvrhdlr->clientindx = clientindx;
+				srvrhdlr->clientlist[clientindx].con_id = ret;
+				srvrhdlr->clientlist[clientindx].status = CLIENTBOX_READY;
+
+				thpool_add_work(srvrhdlr->thpoolinst, (void*)svrproc_communicate, srvrhdlr);
+				/*call connected*/
+				srvrhdlr->connected_cb(&ret, clientaddr.sin_addr.s_addr);
 			}
 			else
 			{
-				/* no any new connection, just wait*/
-				sleep(1);
+				if (errno != EWOULDBLOCK && errno != EAGAIN)
+				{
+					/*system error*/
+					LOG("tsap: accept() got error: %s\n", strerror(errno));
+					break;
+				}
+				else
+				{
+					/* no any new connection, just wait*/
+					sleep(1);
+				}
 			}
 		}
 	}
 }
 
+void svrproc_communicate(psrvrhdlr_t srvrhdlr)
+{
+	clientbox_type myclient;
+	uint32_t	error_code;
+	uint16_t	cnt;
+	START_SEGMENT
+
+	EXIT_IF(srvrhdlr->clientindx >= MAX_CLIENT_SP, error_code = -1)
+	/*save client index for use*/
+	myclient = srvrhdlr->clientlist[srvrhdlr->clientindx];
+	/*response to server*/
+	srvrhdlr->clientindx = MAX_CLIENT_SP;
+
+	while(1)
+	{
+		if (myclient.status == CLIENTBOX_READY)
+		{
+			cnt = recv(myclient.con_id, myclient.rxbuff, MAX_BUFFER_SIZE, 0);
+			if (cnt == -1)
+			{
+				if (errno != EWOULDBLOCK && errno != EAGAIN)
+				{
+					/*system error*/
+					LOG("tsap: recv() got error: %s\n", strerror(errno));
+					break;
+				}
+				else
+				{
+					/* no any data yet, just wait*/
+					sleep(1);
+				}
+			}
+			else if (cnt == 0)
+			{
+				/*connection closed*/
+				break;
+			}
+			else if (cnt > 0)
+			{
+				/*got data, call receive call back*/
+				myclient->rxbuflen = cnt;
+				srvrhdlr->receive_cb(&myclient.con_id, myclient->rxbuff, myclient->rxbuflen);
+			}
+		}
+		else if (myclient.status == CLIENTBOX_TXREQ)
+		{
+			cnt = send(myclient.con_id, myclient->txbuff, myclient->txbuflen);
+			if (cnt == -1)
+			{
+				if (errno != EWOULDBLOCK && errno != EAGAIN)
+				{
+					/*system error*/
+					LOG("tsap: send() got error: %s\n", strerror(errno));
+					break;
+				}
+				else
+				{
+					/* send in process, just wait*/
+					//sleep(1);
+				}
+			}
+			else if (cnt >= 0)
+			{
+				/*done*/
+				myclient.status = CLIENTBOX_READY;
+			}
+
+		}
+		else if (myclient.status == CLIENTBOX_FREE)
+		{
+			break;
+		}
+	}
+	END_SEGMENT
+
+	myclient.status = CLIENTBOX_FREE;
+	srvrhdlr->disconnected_cb(&myclient.status, error_code);
+}
+
+int16_t get_clientbox(clientbox_type* list)
+{
+	uint16_t i;
+	for (i = 0; i<MAX_CLIENT_SP; i++)
+	{
+		if (list[i].status == CLIENTBOX_FREE)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
--- sync/tsap_srvr.h
+++ sync/tsap_srvr.h
@@ -8,16 +8,33 @@
 #ifndef SRC_TSAP_TSAP_SRVR_H_
 #define SRC_TSAP_TSAP_SRVR_H_
 
-#define MAX_CLIENT_SP				10
+#define MAX_CLIENT_SP					10
+
+#define MAX_BUFFER_SIZE					1024
+
+#define CLIENTBOX_FREE					0
+#define CLIENTBOX_READY					1
+#define CLIENTBOX_TXREQ					2
+
 typedef struct
 {
 	uint32_t	con_id;
-
+	uint8_t		status;
+	uint8_t		rxbuff[MAX_BUFFER_SIZE];
+	uint8_t		txbuff[MAX_BUFFER_SIZE];
+	uint16_t	rxbuflen;
+	uint16_t	txbuflen;
 } clientbox_type;
 
 typedef struct
 {
 	uint32_t				socketid;
+	threadpool				thpoolinst;
+	void (*connected_cb)(void* con_id, uint32_t client_ip);
+	void (*disconnected_cb)(void* con_id, uint32_t error_code);
+	void (*receive_cb)(void* con_id, void* mesg, uint16_t len);
+	uint16_t				clientindx;
+	clientbox_type			clientlist[MAX_CLIENT_SP];
 } serverhandler_type;
 
 typedef serverhandler_type* 	psrvrhdlr_t;
